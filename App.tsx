53de87a49e8d098c52cb2bd63df97c0eaca422a0
tokatipan1223 <katouichirou537@gmail.com>
2025-07-02 07:50:36 +0900

    Add calendar screen with event management

--- App.tsx
+++ App.tsx
@@ -1,12 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
+import React from 'react';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import CalendarScreen from './src/CalendarScreen';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -14,7 +8,7 @@ function App() {
   return (
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <CalendarScreen />
     </View>
   );
 }
